67f9a56605a0882b1e8aae4c6f4b365eabcd97d5
LSHummel <lshummel13@gmail.com>
2026-04-28 20:17:20 -0300

    Esqueleto do menu

--- Simulador.c
+++ Simulador.c
@@ -1,17 +1,35 @@
 #include<stdio.h>
 
 void menu(void){
-
-
+printf("=========================");
+printf("   RECARGA DE BATERIA");
+printf("=========================");
+printf("1 - (entrada de informações)");
+printf("n - Tempo estimado de recarga");
+printf("n - Energia consumida");
+printf("n - Custo da recarga");
+printf("x - Fim da Simulacao");
+printf("Escolha uma opcao: ");
 }
 
 
 
 
 int main(){
-menu();
 
+int opcao, x;
+while(opcao != x){
 
+    menu();
+    scanf("%d", opcao);
 
+    switch(opcao){
+        case 1:
+            break;
+        case 2:
+            break;
+    }
+
+}
     return 0;
 }
