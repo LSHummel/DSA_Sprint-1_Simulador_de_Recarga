c6e9adddd393b6d51fb518c468206085cb5510f5
LSHummel <lshummel13@gmail.com>
2026-04-28 21:14:47 -0300

    Esqueleto 2.0

--- Simulador.c
+++ Simulador.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
 void menu(void){
-printf("=========================");
-printf("   RECARGA DE BATERIA");
-printf("=========================");
-printf("1 - (entrada de informações)");
-printf("n - Tempo estimado de recarga");
-printf("n - Energia consumida");
-printf("n - Custo da recarga");
-printf("x - Fim da Simulacao");
+printf("=========================\n");
+printf("   RECARGA DE BATERIA\n");
+printf("=========================\n");
+printf("1 - (entrada de informacoes)\n");
+printf("2 - Tempo estimado de recarga\n");
+printf("3 - Energia consumida\n");
+printf("4 - Custo da recarga\n");
+printf("5 - Fim da Simulacao\n");
 printf("Escolha uma opcao: ");
 }
 
@@ -17,16 +17,27 @@ printf("Escolha uma opcao: ");
 
 int main(){
 
-int opcao, x;
-while(opcao != x){
+int opcao;
+while(opcao != 5){
 
     menu();
-    scanf("%d", opcao);
+    scanf("%d", &opcao);
 
     switch(opcao){
         case 1:
+        printf("Opcao 1\n");
             break;
         case 2:
+        printf("Opcao 2\n");
+            break;
+        case 3:
+        printf("Opcao 3\n");
+            break;
+        case 4:
+        printf("Opcao 4\n");
+            break;
+        case 5:
+        printf("Fim da Simulacao");
             break;
     }
 
