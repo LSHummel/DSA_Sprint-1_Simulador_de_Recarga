221b0da278825dab3ad8fc02311474e2b8d0cf4c
LSHummel <lshummel13@gmail.com>
2026-04-29 09:50:48 -0300

    Cases adicionados

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# DSA_Sprint-1_Simulador_de_Recarga
+# Sprint-1 Simulador de Recarga
+
+## Informações
+- Carregador rápido (DC 50 kW), 60 minutos
+- Custo R$2,00 por kWh, Taxa de ativação R$1,80
+
+## Referência
+https://www.greenv.com.br/blog/quanto-tempo-demora-para-carregar-carro-eletrico-em-eletroposto/#:~:text=Em%20m%C3%A9dia%2C%20quanto%20tempo%20demora,e%20do%20modelo%20do%20ve%C3%ADculo.
+
--- Simulador.c
+++ Simulador.c
@@ -5,8 +5,8 @@ printf("=========================\n");
 printf("   RECARGA DE BATERIA\n");
 printf("=========================\n");
 printf("1 - (entrada de informacoes)\n");
-printf("2 - Tempo estimado de recarga\n");
-printf("3 - Energia consumida\n");
+printf("2 - Energia consumida\n");
+printf("3 - Tempo estimado de recarga\n");
 printf("4 - Custo da recarga\n");
 printf("5 - Fim da Simulacao\n");
 printf("Escolha uma opcao: ");
@@ -18,26 +18,47 @@ printf("Escolha uma opcao: ");
 int main(){
 
 int opcao;
+float total_kwh, ener_consumida, porcentagem, valor_total, tempo;
+
+
 while(opcao != 5){
 
     menu();
     scanf("%d", &opcao);
-
+    printf("\n");
     switch(opcao){
         case 1:
-        printf("Opcao 1\n");
+        printf("=========================\n");
+        printf("Quantos kWh cabem no carro: \n");
+        scanf("%f", &total_kwh);
+        printf("Porcentagem de energia do carro: \n");
+        scanf("%f", &porcentagem);
+        printf("=========================\n");
+        printf("\n");
             break;
         case 2:
-        printf("Opcao 2\n");
+        ener_consumida = total_kwh - total_kwh*(porcentagem/100);
+        printf("=========================\n");
+        printf("A energia consumida pelo carro e de: %.2f kWh\n", ener_consumida);
+        printf("=========================\n");
+        printf("\n");
             break;
         case 3:
-        printf("Opcao 3\n");
+        tempo = (ener_consumida * 60) / 50;
+        printf("=========================\n");
+        printf("O tempo de regarda e de %.2f minutos\n", tempo); // carregador rapido (DC 50 Kw) 100% em 1 hora
+        printf("=========================\n");
+        printf("\n");
             break;
         case 4:
-        printf("Opcao 4\n");
+        valor_total = ener_consumida * 2 + 1.80; // preço por kwh = R$2 ; taxa de ativação = R$1,80 
+        printf("=========================\n");
+        printf("O custo total do abastecimento e de: R$%.2f\n", valor_total); // total = (kWh adicionado) X (Preço por kWh) + Taxa de ativação
+        printf("=========================\n");
+        printf("\n");
             break;
         case 5:
-        printf("Fim da Simulacao");
+        printf("---- Fim da Simulacao ----");
             break;
     }
 
