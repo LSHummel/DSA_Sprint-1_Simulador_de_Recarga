3fe7606670dfe860c9cf3a86c1789ac65bd8949a
LSHummel <lshummel13@gmail.com>
2026-05-12 14:54:54 -0300

    voltar ao menu

--- Simulador.c
+++ Simulador.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 void menu(void){
-printf("=========================\n");
-printf("   RECARGA DE BATERIA\n");
-printf("=========================\n");
+printf("================================\n");
+printf("    MENU - RECARGA DE BATERIA\n");
+printf("================================\n");
 printf("1 - Informacoes do carro\n");
 printf("2 - Energia consumida\n");
 printf("3 - Tempo estimado de recarga\n");
@@ -18,7 +18,7 @@ printf("Escolha uma opcao: ");
 
 int main(){
 
-int opcao;
+int opcao, fechar;
 float total_kwh, ener_consumida, porcentagem, valor_total, tempo, porcentagem_f;
 
 
@@ -30,6 +30,8 @@ while(opcao != 6){
     switch(opcao){
         case 1:
         printf("=========================\n");
+        printf("  Informacoes do carro\n");
+        printf("=========================\n");
         printf("Quantos kWh cabem no carro: \n");
         scanf("%f", &total_kwh);
         printf("Porcentagem de energia do carro: \n");
@@ -38,7 +40,16 @@ while(opcao != 6){
                 printf("A porcentagem esta errada, digite novamente (0 ate 100): \n");
                 scanf("%f", &porcentagem);
             }
-        printf("=========================\n");
+        printf("Para voltar ao menu digite 0\n");
+        scanf("%d", &fechar);
+        while(fechar != 0){
+            printf("Para voltar ao menu digite 0\n");
+            scanf("%d", &fechar);
+            switch(fechar){
+                case 0:
+                    break;}
+            }
+        printf("-----------------------------\n");
         printf("\n");
         // calculos
         ener_consumida = total_kwh - total_kwh*(porcentagem/100); // energia consumida pelo carro para carregar 100%
@@ -48,20 +59,56 @@ while(opcao != 6){
             break;
         case 2:
         printf("=========================\n");
-        printf("A energia consumida pelo carro e de: %.2f kWh\n", ener_consumida);
+        printf("    Energia consumida\n");
         printf("=========================\n");
+        printf("A energia consumida pelo carro e de: %.2f kWh\n", ener_consumida);
+        printf("\n");
+        printf("Para voltar ao menu digite 0\n");
+        scanf("%d", &fechar);
+        while(fechar != 0){
+            printf("Para voltar ao menu digite 0\n");
+            scanf("%d", &fechar);
+            switch(fechar){
+                case 0:
+                    break;}
+            }
+        printf("-----------------------------\n");
         printf("\n");
             break;
         case 3:
         printf("=========================\n");
-        printf("O tempo de regarda e de %.2f minutos\n", tempo); // carregador rapido (DC 50 Kw) 100% em 1 hora
+        printf("Tempo estimado de recarga\n"); // carregador rapido (DC 50 Kw) 100% em 1 hora
         printf("=========================\n");
+        printf("O tempo de regarda e de %.2f minutos\n", tempo);
+        printf("\n");
+        printf("Para voltar ao menu digite 0\n");
+        scanf("%d", &fechar);
+        while(fechar != 0){
+            printf("Para voltar ao digite 0\n");
+            scanf("%d", &fechar);
+            switch(fechar){
+                case 0:
+                    break;}
+            }
+        printf("-----------------------------\n");
         printf("\n");
             break;
         case 4:
         printf("=========================\n");
-        printf("O custo total do abastecimento e de: R$%.2f\n", valor_total); // total = (kWh adicionado) X (Preço por kWh) + Taxa de ativação
+        printf("    Custo da recarga\n"); // total = (kWh adicionado) X (Preço por kWh) + Taxa de ativação
         printf("=========================\n");
+        printf("O custo total do abastecimento e de: R$%.2f\n", valor_total);
+        printf("\n");
+        printf("Para voltar ao menu digite 0\n");
+        scanf("%d", &fechar);
+        while(fechar != 0){
+            printf("Para voltar ao menu digite 0\n");
+            scanf("%d", &fechar);
+            switch(fechar){
+                case 0:
+                    break;}
+            }
+        printf("-----------------------------\n");
         printf("\n");
             break;
         case 5:
@@ -73,6 +120,17 @@ while(opcao != 6){
         printf("Energia consumida pelo carro: %.2f kWh\n", ener_consumida);
         printf("Tempo estimado de regarda: %.2f minutos\n", tempo);
         printf("Custo total do abastecimento: R$%.2f\n", valor_total);
+        printf("\n");
+        printf("Para voltar ao menu digite 0\n");
+        scanf("%d", &fechar);
+        while(fechar != 0){
+            printf("Para voltar ao menu digite 0\n");
+            scanf("%d", &fechar);
+            switch(fechar){
+                case 0:
+                    break;}
+            }
+        printf("-----------------------------\n");
         printf("\n");
             break;
         case 6:
